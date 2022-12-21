c96d17f4c2cbcda8bed1d6b4a36c77dfd8a3ea52
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2022-12-21 13:11:29 +0530

    all functions working

---
+++ script.js
@@ -0,0 +1,50 @@
+let addBtn = document.querySelector(`.addBtn`);
+// let deleteBtn = document.querySelector(`.btnDelete`);
+
+addBtn.addEventListener(`click`, () => {
+    // read the task entered by user
+    let userTask = document.querySelector(`.userTask`).value;
+
+    const newTask = document.createElement("div");
+    const newBtnEdit = document.createElement("button");
+    const newBtnDelete = document.createElement("button");
+    const newToDoTask = document.createElement("input");
+
+    // newTask.textContent = "new task through DOM"
+    document.querySelector(`.taskContainer`).appendChild(newTask);
+
+    newTask.classList.add(`tasks`, `p-2`, `flex`, `justify-center`);
+
+    newTask.appendChild(newToDoTask);
+    newToDoTask.value = userTask.toString();
+    newToDoTask.setAttribute("readonly", "readonly");
+    newToDoTask.classList.add(`p-2`);
+
+    newBtnEdit.textContent = "EDIT";
+    newBtnEdit.classList.add(`btnEdit`, `bg-green-400`, `w-24`, `rounded-md`, `ml-5`);
+    newTask.appendChild(newBtnEdit);
+
+    newBtnDelete.textContent = "Delete";
+    newBtnDelete.classList.add(`btnDelete`, `bg-red-400`, `w-24`, `rounded-md`, `ml-5`);
+    newTask.appendChild(newBtnDelete);
+
+    document.querySelector(`.userTask`).value = ``;
+    document.querySelector(`.userTask`).focus();
+
+    newBtnDelete.addEventListener("click", () => {
+        console.log(`Delete Click Test Successful`);
+        document.querySelector(`.taskContainer`).removeChild(newTask);
+    });
+
+    newBtnEdit.addEventListener(`click`, () => {
+        console.log(`Edit Click Test Ok`);
+        if (newBtnEdit.textContent == `EDIT`) {
+            newToDoTask.removeAttribute("readonly");
+            newToDoTask.focus();
+            newBtnEdit.textContent = `SAVE`;
+        } else {
+            newToDoTask.setAttribute("readonly", "readonly");
+            newBtnEdit.textContent = `EDIT`;
+        }
+    });
+});
